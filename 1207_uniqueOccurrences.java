e06ebaa27ed7c5fc84637cc05c5d1d39502539d0
Santhosh M <143023757+SanthoshM007@users.noreply.github.com>
2024-12-06 19:58:36 +0530

    Create 1207_uniqueOccurrences.java

---
+++ 1207_uniqueOccurrences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        
+
+        for(int i=0;i<arr.length;i++){
+
+            if(!map.containsKey(arr[i])) map.put(arr[i],1);
+
+            else map.put(arr[i],map.get(arr[i])+1);
+
+        }
+        Set<Integer> uni = new HashSet<>();
+       
+
+        for (Integer val : map.values()) {
+           uni.add(val);
+        }
+
+
+        return (uni.size()==map.size()) ? true:false;
+    }
+}
